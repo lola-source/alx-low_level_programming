ea3369af0b96b6124005f0f35b45fbfc3b314a8b
lola-source <adagaladolores@gmail.com>
2022-03-10 13:13:00 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,10 @@
-/*
- * file: 5-printf.c
- * auth: lola-source
- */
 #include <stdio.h>
 /**
- * main - prints "with proper grammar
- *		but the outcome is a piece of art" followed by a line
- *return: always 0.
+ *main - main block
+ *Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }

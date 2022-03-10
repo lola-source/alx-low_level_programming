a48867f472a51a843ebd1a08ee879626c6a8dd1f
lola-source <adagaladolores@gmail.com>
2022-03-10 13:41:13 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,20 @@
 /*
- * file:101-quote.c
- * auth:lola-source
+ *File: 101-quote.c
+ *Auth:lola-source
  */
-#include<unistd.h>
+
+#include <unistd.h>
+
 /**
- *main - print"and that piece of art is useful"-Dora Kopar, 2015-10-19",
- *	followed by a line, to the standard error.
- *return: always 1.
+ *main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error."
+ *
+ *Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful" -Dora Kopar, 2015-10-19\n",
-		59);
-	return(1);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
+	return (1);
 }

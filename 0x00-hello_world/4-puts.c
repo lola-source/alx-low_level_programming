70f84decd4e710563267f33e0cea048c86f9ec88
lola-source <adagaladolores@gmail.com>
2022-03-10 13:08:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-#!/bin/bash
-/*
- *file:4-puts.c
- *auth:lola-source
- */
 #include <stdio.h>
 /**
- *main -Print "Programming is like building a 
- *		multilingual puzzle"followed by a line
- *return:always 0.
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }

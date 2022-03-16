26d28d242761c1414540aa4ea653377f0787a1c6
lola-source <adagaladolores@gmail.com>
2022-03-15 19:31:37 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,47 @@
 #include "main.h"
 /**
- *_isalpha - checks for alphabetic character
- *@c: the character to be checked
- *Return: 1 if c is a letter, 0 otherwise
+ *print_times_table - prints times table
+ *@n : times table to use
+ *Return:void
  */
-int _isalpha(int c)
+void print_times_table(int n)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	int x = 0, rep, y;
+
+	if (n < 0 || n > 15)
+	return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			rep = x * y;
+			if (y == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (y < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+}
 }

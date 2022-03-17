0587fa46fab85dd6ca219dcd3a2ede6a59d473a4
lola-source <adagaladolores@gmail.com>
2022-03-17 15:41:12 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  *main - tests function that prints if integer is positive or negative
- *Return: 0
+ *@i - character to check positive_or_negative
+ * Return: 0
  */
 int main(void)
 {
@@ -9,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }

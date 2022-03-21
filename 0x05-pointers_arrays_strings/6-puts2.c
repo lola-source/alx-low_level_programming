ea751d002467e01fb8b42609cbf7be1b87a0b523
lola-source <adagaladolores@gmail.com>
2022-03-21 10:42:52 -0700

    every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a  string to stdout\
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
  * Return: void
  */
@@ -10,8 +10,8 @@ void puts2(char *str)
 
 	while (*(str + i))
 	{
-	_putchar(*(str + i));
-	i = i + 2;
+		_putchar(*(str + i));
+		i = i + 2;
 	}
 	_putchar('\n');
 }

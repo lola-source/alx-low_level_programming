11135a8ada239e1a3abe402d7980b335cfcd8e61
lola-source <adagaladolores@gmail.com>
2022-03-24 15:06:32 -0700

    red pill

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,28 @@
 #include "main.h"
 /**
- * _strcat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
- *
- * Return: address of dest
+ * *_strcat - function commute strings
+ * @dest : param pointer to a char
+ * @src : param pointer to a char
+ * return : return value of dest
  */
-char *_strcat(char *dest, char *src)
+char *strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
 		i++;
-		j++;
 	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }

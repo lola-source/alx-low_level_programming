6c1b99dfe3c670aae24168f2de800071ca8e6424
lola-source <adagaladolores@gmail.com>
2022-04-28 05:54:22 -0700

    commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,10 @@
+ #include "main.h"
+int get_endianness(void)
+{
+	int x;
+	char *y;
+
+	x = 1;
+	y = (char *) &x;
+	return (*y);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	set = ~(1 << index);
+	*n = *n & set;
+	return (1);
+}
